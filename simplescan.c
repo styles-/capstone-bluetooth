a735d0f12faf5ff93c24d263716d7dee1bd6f92b
cprice <cprice@intelliair.com>
2016-03-09 10:35:37 -0600

    simplescan now added

---
+++ simplescan.c
@@ -0,0 +1,48 @@
+//
+//  simplescan.c
+//  
+//
+//  Created by Cody Price on 3/9/16.
+//
+//
+
+#include "simplescan.h"
+
+int main(int argc, char **argv) {
+    
+    inquiry_info *devices = NULL;
+    int max_rsp, num_rsp;
+    int adapter_id, sock, len, flags;
+    int i;
+    char addr[19] = { 0 };
+    char name[248] = { 0 };
+    
+    adapter_id = hci_get_route(NULL);
+    sock = hci_open_dev( adapter_id );
+
+    if (adapter_id < 0 || sock < 0) {
+        perror("Opening socket..");
+        exit(EXIT_FAILURE);
+    }
+    
+    len = 8;
+    max_rsp = 255;
+    flags = IREQ_CACHE_FLUSH;
+    devices = (inquiry_info*) malloc(max_rsp * sizeof(inquiry_info));
+    
+    num_rsp = hci_inquiry(adapter_id, len, max_rsp, NULL, &devices, flags);
+    if ( num_rsp < 0 )
+        perror("hci_inquiry");
+    
+    for (i = 0; i < num_rsp; ++i) {
+        ba2str(&(devices+i)->baddr, addr);
+        memset(name, 0, sizeof(name));
+        if (0 != hci_read_remote_name(sock, &(devices+i)->baddr, sizeof(name), name, 0))
+            strcpy(name, "[unknown]");
+        printf("%s | %s\n", addr, name);
+    }
+    
+    free( devices );
+    close( sock );
+    return EXIT_SUCCESS;
+}
---
+++ simplescan.h
@@ -0,0 +1,20 @@
+//
+//  simplescan.h
+//  
+//
+//  Created by Cody Price on 3/9/16.
+//
+//
+
+#ifndef simplescan_h
+#define simplescan_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+
+#endif /* simplescan_h */
