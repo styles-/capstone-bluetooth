49c5995b95f304bd29e6d0c2ecfdb6a42579d486
cprice <cprice@intelliair.com>
2016-03-09 17:02:56 -0600

    added addresses

--- simplescan.h
+++ simplescan.h
@@ -17,4 +17,11 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
+#define ADDR_NEXUS_7 "AC:22:0B:46:EA:EB"
+#define ADDR_CODY_PHONE "22:22:6F:E2:50:44"
+#define ADDR_CODY_LAPTOP "A0:99:9B:13:05:04"
+#define ADDR_HC_05 "20:15:04:30:74:62"
+#define ADDR_BT_PLUG_1 "5C:F3:70:76:BE:99"
+#define ADDR_BT_PLUG_2 ""
+
 #endif /* simplescan_h */
