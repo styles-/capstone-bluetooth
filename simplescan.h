b523ba2e608abbfe844bc08104bca4911cd3a926
cprice <cprice@intelliair.com>
2016-03-12 22:49:15 -0600

    added bt plug 2 address

--- simplescan.h
+++ simplescan.h
@@ -22,6 +22,6 @@
 #define ADDR_CODY_LAPTOP "A0:99:9B:13:05:04"
 #define ADDR_HC_05 "20:15:04:30:74:62"
 #define ADDR_BT_PLUG_1 "5C:F3:70:76:BE:99"
-#define ADDR_BT_PLUG_2 ""
+#define ADDR_BT_PLUG_2 "20:15:04:30:74:62"
 
 #endif /* simplescan_h */
