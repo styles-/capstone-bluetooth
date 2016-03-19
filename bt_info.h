b3aeeee78911dcb177d9f2632715b71fc55d1aeb
cprice <cprice@intelliair.com>
2016-03-18 22:38:06 -0500

    update bluetooth addresses

--- bt_info.h
+++ bt_info.h
@@ -9,8 +9,9 @@
 #ifndef bt_info_h
 #define bt_info_h
 
-#define ADDR_NEXUS_7 "AC:22:0B:46:EA:EB"
-#define ADDR_CODY_PHONE "22:22:6F:E2:50:44"
+#define ADDR_CODY_NEXUS7 "AC:22:0B:46:EA:EB"
+#define ADDR_CODY_NEXUS5X "88:07:4B:37:0E:F2"
+#define ADDR_CODY_LGG3 "22:22:6F:E2:50:44"
 #define ADDR_CODY_LAPTOP "A0:99:9B:13:05:04"
 #define ADDR_HC_05 "20:15:04:30:74:62"
 #define ADDR_BT_PLUG_1 "5C:F3:70:76:BE:99"
