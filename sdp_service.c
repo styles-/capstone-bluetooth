264a2c0ac2005fb64e155f3a46850bf8f8095a8f
cprice <cprice@intelliair.com>
2016-03-25 02:03:17 -0500

    testing..

--- sdp_service.c
+++ sdp_service.c
@@ -28,28 +28,33 @@ sdp_session_t* register_service() {
                 *profile_list = 0;
     sdp_data_t *channel = 0;
     sdp_profile_desc_t profile;
-    sdp_record_t record = { 0 };
+//    sdp_record_t record = { 0 };
+    sdp_record_t *record = sdp_record_alloc();
     sdp_session_t *session = 0;
     
     // set the general service ID
     sdp_uuid128_create( &svc_uuid, &svc_uuid_int );
-    sdp_set_service_id( &record, svc_uuid );
+//    sdp_set_service_id( &record, svc_uuid );
+    sdp_set_service_id( record, svc_uuid );
     
     // set the service class
     sdp_uuid16_create( &svc_class_uuid, SERIAL_PORT_SVCLASS_ID );
     svc_class_list = sdp_list_append(0, &svc_class_uuid);
-    sdp_set_service_classes(&record, svc_class_list);
+//    sdp_set_service_classes(&record, svc_class_list);
+    sdp_set_service_classes(record, svc_class_list);
     
     // set the Bluetooth profile information
     sdp_uuid16_create(&profile.uuid, SERIAL_PORT_PROFILE_ID);
     profile.version = 0x0100;
     profile_list = sdp_list_append(0, &profile);
-    sdp_set_profile_descs(&record, profile_list);
+//    sdp_set_profile_descs(&record, profile_list);
+    sdp_set_profile_descs(record, profile_list);
     
     // make the service record publicly browsable
     sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
     root_list = sdp_list_append(0, &root_uuid);
-    sdp_set_browse_groups( &record, root_list );
+//    sdp_set_browse_groups( &record, root_list );
+    sdp_set_browse_groups( record, root_list );
     
     // set l2cap information
     sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
@@ -64,14 +69,16 @@ sdp_session_t* register_service() {
     sdp_list_append( proto_list, rfcomm_list );
     
     access_proto_list = sdp_list_append( 0, proto_list );
-    sdp_set_access_protos( &record, access_proto_list );
+//    sdp_set_access_protos( &record, access_proto_list );
+    sdp_set_access_protos( record, access_proto_list );
     
     // set the name, provider, and description
-    sdp_set_info_attr(&record, service_name, service_prov, service_desc);
+//    sdp_set_info_attr(&record, service_name, service_prov, service_desc);
+    sdp_set_info_attr(record, service_name, service_prov, service_desc);
     
     // connect to the local SDP server, register the service record, and disconnect
     session = sdp_connect(BDADDR_ANY, BDADDR_LOCAL, SDP_RETRY_IF_BUSY);
-    sdp_record_register(session, &record, 0);
+//    sdp_record_register(session, &record, 0);
     
     // cleanup
     sdp_data_free( channel );
