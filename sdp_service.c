5111ac4a53b38378a5c48b3b016d0eb991f7af81
cprice <cprice@intelliair.com>
2016-04-04 04:09:23 -0500

    fix rfcomm_channel

--- sdp_service.c
+++ sdp_service.c
@@ -11,99 +11,6 @@
 
 //#define UUID_STR "9159BA6D-F5BA-43C6-8C9B-0E318CBD1809"
 
-
-/*
-sdp_session_t* register_service() {
-    uint32_t svc_uuid_int[] = { 0x9159BA6D, 0xF5BA43C6, 0x8C9B0E31, 0x8CBD1809 };
-    uint8_t rfcomm_channel = 11;
-    const char *service_name = "Test SDP Service";
-    const char *service_desc = "An experimental sdp service";
-    const char *service_prov = "CodyPrice";
-    
-    uuid_t root_uuid, l2cap_uuid, rfcomm_uuid, svc_uuid, svc_class_uuid;
-    sdp_list_t  *l2cap_list = 0,
-                *rfcomm_list = 0,
-                *root_list = 0,
-                *proto_list = 0,
-                *access_proto_list = 0,
-                *svc_class_list = 0,
-                *profile_list = 0;
-    sdp_data_t *channel = 0;
-    sdp_profile_desc_t profile;
-//    sdp_record_t record = { 0 };
-    sdp_record_t *record = sdp_record_alloc();
-    sdp_session_t *session = 0;
-    
-    // set the general service ID
-    sdp_uuid128_create( &svc_uuid, &svc_uuid_int );
-//    sdp_set_service_id( &record, svc_uuid );
-    sdp_set_service_id( record, svc_uuid );
-    
-    // set the Service class ID
-//    sdp_list_t service_class = { NULL, &svc_uuid };
-//    sdp_set_service_classes
-    
-    // set the service class
-    sdp_uuid16_create( &svc_class_uuid, SERIAL_PORT_SVCLASS_ID );
-    svc_class_list = sdp_list_append(0, &svc_class_uuid);
-//    sdp_set_service_classes(&record, svc_class_list);
-    sdp_set_service_classes(record, svc_class_list);
-    
-    // set the Bluetooth profile information
-    sdp_uuid16_create(&profile.uuid, SERIAL_PORT_PROFILE_ID);
-    profile.version = 0x0100;
-    profile_list = sdp_list_append(0, &profile);
-//    sdp_set_profile_descs(&record, profile_list);
-    sdp_set_profile_descs(record, profile_list);
-    
-    // make the service record publicly browsable
-    sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
-    root_list = sdp_list_append(0, &root_uuid);
-//    sdp_set_browse_groups( &record, root_list );
-    sdp_set_browse_groups( record, root_list );
-    
-    // set l2cap information
-    sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
-    l2cap_list = sdp_list_append( 0, &l2cap_uuid );
-    proto_list = sdp_list_append( 0, l2cap_list );
-    
-    // register the RFCOMM channel for RFCOMM sockets
-    sdp_uuid16_create(&rfcomm_uuid, RFCOMM_UUID);
-    channel = sdp_data_alloc(SDP_UINT8, &rfcomm_channel);
-    rfcomm_list = sdp_list_append( 0, &rfcomm_uuid );
-    sdp_list_append( rfcomm_list, channel );
-    sdp_list_append( proto_list, rfcomm_list );
-    
-    access_proto_list = sdp_list_append( 0, proto_list );
-//    sdp_set_access_protos( &record, access_proto_list );
-    sdp_set_access_protos( record, access_proto_list );
-    
-    // set the name, provider, and description
-//    sdp_set_info_attr(&record, service_name, service_prov, service_desc);
-    sdp_set_info_attr(record, service_name, service_prov, service_desc);
-    
-    // connect to the local SDP server, register the service record, and disconnect
-    session = sdp_connect(BDADDR_ANY, BDADDR_LOCAL, SDP_RETRY_IF_BUSY);
-    if (session == (sdp_session_t *) NULL) {
-        fprintf(stderr, "Error: Device not found\n");
-        exit(EXIT_FAILURE);
-    }
-//    sdp_record_register(session, &record, 0);
-    sdp_record_register(session, record, 0);
-    
-    // cleanup
-    sdp_data_free( channel );
-    sdp_list_free( l2cap_list, 0 );
-    sdp_list_free( rfcomm_list, 0 );
-    sdp_list_free( root_list, 0 );
-    sdp_list_free( access_proto_list, 0 );
-    sdp_list_free( svc_class_list, 0 );
-    sdp_list_free( profile_list, 0 );
-    
-    return session;
-}
-*/
-
 sdp_session_t *register_service(const uint8_t rfcomm_channel) {
     int32_t svc_uuid_int[] = { 0x9159BA6D, 0xF5BA43C6, 0x8C9B0E31, 0x8CBD1809 };
     //uint32_t svc_uuid_int[] = { 0x00001101, 0x00001000, 0x80000080, 0x5f9b34fb };
@@ -155,7 +62,7 @@ sdp_session_t *register_service(const uint8_t rfcomm_channel) {
     
     // register the RFCOMM channel for RFCOMM sockets
     sdp_uuid16_create(&rfcomm_uuid, RFCOMM_UUID);
-    channel = sdp_data_alloc(SDP_UINT8, &rfcomm_uuid);
+    channel = sdp_data_alloc(SDP_UINT8, &rfcomm_channel);
     rfcomm_list = sdp_list_append(0, &rfcomm_uuid);
     sdp_list_append(rfcomm_list, channel);
     sdp_list_append(proto_list, rfcomm_list);
